04a808cd8f7d54ca16c3faa433bb4c460eb4196b
justinjoliver <justin.j.lee3411@gmail.com>
2023-12-15 17:39:53 -0600

    Projection update as of 12.15.2023

--- source/bvar/bvar_exec.jl
+++ source/bvar/bvar_exec.jl
@@ -1,3 +1,11 @@
+# Bayesian VAR model with Minnesota prior
+#   Data sample and code transaltion from source 
+# NOTE on transaltion
+#   Cholesky depcomposition, by default on MATLAB, is upper-triangle
+#       the simpler-hard-coded cholesky function (with hermitian depednece)
+#       is coded in lower-triangle format:: transpose needed
+#   Cholesky factorization needs computation of Hermitian form
+#       this includes any distribtuion approach (Multinomial distribtuion)
 using Pkg
 #using Distributed#, GPUArrays
 #addprocs(5)
@@ -11,25 +19,25 @@ using CSV, Dates
 using JLD
 Random.seed!(1234)
 using Plots
+
+# Set working directory
 cd(@__DIR__)
+
 # Data location in application
 str_dir_git = splitdir(splitdir(pwd())[1])[1]
-# Pull bvar functions 
-include(str_dir_git*"/source/bvar/functions.jl")
-# ================================================================================================
-# Data import from FRED to be updated soon
-#   Dataframe and format:
-#       Christiano et al.: 
-#       Phillips and Okun's Law (job opening should follow my prior package)
-#       Stock market and uncertainty
-# ================================================================================================
-# Data update and save
-cd(@__DIR__)
-include("./data/proc_data_macro.jl")
-# ================================================================================================
-# Fitness 
-# ................................................................................................
- 
+
+include("functions.jl")
+# VAR using the Gibbs sampler, based on independent Normal Wishar prior
+
+#------------------------------LOAD DATA-----------------------------------
+# Load Quarterly US data on inflation, unemployment and interest rate, 
+# 1953:Q1 - 2006:Q3
+# Data: inflation, unemployment and interest rate
+#Yraw = DataFrame(readdlm("Yraw.dat"), [:inf, :unemp, :ffr])
+#Yraw = Matrix(Yraw)
+#Yraw = Yraw[2:end, :] - Yraw[1:end-1, :]
+#str_varname = ["PCE Inflation", "Unemployment", "Federal Funds Rate"]
+
 # Data import
 str_dataloc = str_dir_git*"/applications/bvar_macro/data/df_wk.csv"
 dfoj = CSV.read(str_dataloc, DataFrame)
@@ -50,10 +58,7 @@ ỹ = dfmod[map(!,completecases(dfmod)),:]
 
 # Paramter defined
 T, k = size(y) # Total calibration data size
-# ................................................................................................
-# Fitness 
-# ................................................................................................
- 
+
 # Define parameters
 testparam = bvar_params(
     constant = true,     # true: if you desire intercepts, false: otherwise 
@@ -84,6 +89,10 @@ t_hat = [t[end] + Day(iter * δ_interval) for iter in 1:testparam.h]
 ALPHA_draws, SIGMA_draws, ypred_draws, irf_draws,
     X, Y = bvar_base(Matrix{Float64}(y), testparam)
 
+
+# =============================================================================================================
+# Fitness 
+# .............................................................................................................
 # Posterior mean of parameters:
 ALPHA_mean = dropdims(mean(ALPHA_draws, dims = 1); dims=1); #posterior mean of ALPHA
 SIGMA_mean = dropdims(mean(SIGMA_draws, dims = 1); dims=1); #posterior mean of SIGMA
@@ -94,10 +103,36 @@ yhat_fit_ub = zeros(size(X)[1], size(Y)[2])
 yhat_fit_lb = zeros(size(X)[1], size(Y)[2])
 for iGibbs = 1:testparam.nsave
     yhat_fit[iGibbs,:,:] = X * ALPHA_draws[iGibbs,:,:]
+    # quantile calculations
+end
+yhat_fit_mean = median(yhat_fit, dims = 1)[1,:,:]
+
+# Recover the level 
+
+
+# plotting
+for iter ∈ 1:length(str_varname)
+    iter_varname = str_varname[iter]
+
+    plot(yhat_fit_ub[:,iter]
+        , fillrange = yhat_fit_lb[:,iter]
+        , fillstyle = :/, label = "Estimated confidence band"
+        ,legend=:topleft
+    )
+    # Point estimates
+    plot!(Y[:,iter], color = :black, linestyle = :dash, label = "Actual")
+    # Centre estimate
+    plot!(yhat_fit_mean[:,iter], linecolor = :red, label = "Median Estimate")
+    # Add title
+    title!("Model fitness of variable: $(iter_varname)")
+    # Print chart
+    png("Fitness_$(iter_varname).png")
 end
 
-# quantile calculations
-yhat_fit_mean = median(yhat_fit, dims = 1)[1,:,:]
+
+# Recover the level information
+
+
 
 # =============================================================================================================
 # Projections 
@@ -135,7 +170,6 @@ if df_proj[end,"time"] ∉ plot_yhat_median[:,"time"] #today() ∉ plot_yhat_med
             (plot_yhat_median[:,"time"] .!= df_proj[end,"time"])
             , :]
         df_proj = vcat(df_proj, tempdf)
-    end
 end
 # Save vintage projection by appending the new (if not exists)
 df_proj |> CSV.write(str_dataloc_vint)
@@ -148,24 +182,109 @@ t_lookback = 52 * 2
 plot_y = y[end-t_lookback:end, :]
 plot_y[:, "time"] = t[end-t_lookback:end]
 
+
+
+
 # Plot the projections
 for iter_col in 1:k
     iter_name = names(plot_y)[iter_col]
-    plot(plot_y[:,"time"], plot_y[:,iter_col], label=iter_name, dpi=300)
-    plot!(df_proj[:,"time"], df_proj[:,iter_col], 
-        label="Projection - Vintage", color=:orange,
-        linewidth = 3
-        )
+    plot(plot_y[:,"time"], plot_y[:,iter_col], label=iter_name)
     plot!(plot_yhat_median[:,"time"], plot_yhat_median[:,iter_col],
-        color=:orange, linestyle =:dot,
         ribbon=(
             plot_yhat_median[:,iter_col] .- yhat_fit_lb[:,iter_col], 
             yhat_fit_ub[:,iter_col] .- plot_yhat_median[:,iter_col]),
-        label = "Projection - Latest",fc=:orange, fa=0.3, linewidth = 2
+        label = "Projection",fc=:orange, fa=0.3, linewidth = 4
     )
     savefig(str_dir_git*"/applications/bvar_macro/results/projections/proj_$(iter_name).png")
 end
 
-#(TBD)
-# Generalized impulse response calculation 
-# https://ken-nyholm.com/Matlab/GIRF/GIRF.html#:~:text=Generalised%20impulse%20response%20functions%20(GIRFs,koop%20et%20al%20(1996)).
+
+recorddf = vcat(plot_y, plot_yhat_median)
+recorddf |> CSV.write("record.csv")
+
+
+# =============================================================================================================
+# Sample Evaluation 
+# .............................................................................................................
+# Bootstrap draws ---------------------------------------------------------------------------------------------
+global boot_A = zeros(testparam.nsave, testparam.constant+(testparam.p*k), k, boot_n)
+global boot_Σ = zeros(testparam.nsave, k, k, boot_n)
+global boot_ŷ = zeros(testparam.nsave, testparam.h, k, boot_n)
+global boot_ϕ = zeros(testparam.nsave, k, k, testparam.ihor, boot_n)
+# Below should be distributed computing 
+
+# Bootstrap iteration
+#@sync @distributed 
+print("Bootstrap Sampling Calibration")
+for iter_boot in 1:boot_n
+    # Randomly select the sample period on bootstratp
+    # Starting with the sample period
+    #   Preserving the least required bootstrap
+    boot_t_start = rand(1:(size(y)[1] - boot_t + 1))
+    boot_t_end   = boot_t_start + boot_t - 1
+    # Select the period sought
+    y_boot = y[boot_t_start:boot_t_end, :]
+
+    # Model build
+    ALPHA_boot, SIGMA_boot, ypred_boot,# irf_boot,
+        X_boot, Y_boot = bvar_base(Matrix{Float64}(y_boot), testparam)
+
+    # Save the result - iteration 
+    global boot_A[:,:,:,iter_boot] = ALPHA_boot
+    global boot_Σ[:,:,:,iter_boot] = SIGMA_boot
+    global boot_ŷ[:,:,:,iter_boot] = ypred_boot
+    #global boot_ϕ[:,:,:,:,iter_boot] = irf_boot
+end
+
+# Save to file ------------------------------------------------------------------------------------------------
+save(str_dir_git*"/applications/bvar_macro/results/simulation.jld",
+    "ALPHA_draws", ALPHA_draws,
+    "SIGMA_draws", SIGMA_draws, 
+    "ypred_draws", ypred_draws,
+    "irf_draws", irf_draws,
+    "boot_A", boot_A,
+    "boot_Σ", boot_Σ,
+    "boot_ŷ", boot_ŷ,
+    "boot_ϕ", boot_ϕ
+)
+
+
+# Development 
+
+params = testparam
+function postmodel_process(, params)
+    # Post process
+
+    # Coefficient draw plots
+    #   Distribution for full sample
+    #   Distribution for the lastest sample 
+    #   Distribution for the bootstrap sample
+
+    # Forecast projection 
+
+end
+
+# ====================== End Sampling Posteriors ===========================
+
+
+
+# Forecasting
+# (1) All-time usage 
+# (2) Bootstrap trained parameter
+# (3) Most recent last couple years trained parameter forecast
+
+if forecasting
+    yhat_mean = dropdims(mean(yhat_draws; dims=1); dims=1)
+    logp_mean = mean(log.(logp_draws))
+
+    # at T+h
+    if forecast_method == 0
+        global true_value = Y1[T+1,:];
+    elseif forecast_method == 1
+        global true_value = Y1[T+h,:];
+    end
+end
+plot!([Yraw[p+1:end,2], yhat_fit_mean[:,2]])
+
+
+# Scenario planning?
